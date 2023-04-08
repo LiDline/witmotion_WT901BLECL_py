47a6930dce52bdbbce4b888e157d25e88256bd7a
LiDline <artyom.zhevnerenko@yandex.ru>
2023-04-08 21:30:04 +0300

    добавлено содержание вклдки About

--- pages/about.py
+++ pages/about.py
@@ -3,15 +3,20 @@ from dash import html
 import re
 
 
-from func.components.app_content import header_page
-
-
 dash.register_page(__name__, path='/')
 
 layout = html.Div([
-    header_page(re.sub(r'\w+\.', '', __name__)),
-
-    html.Div(children='''
+    html.H1(children='''
         This is about page content.
-    ''')
+    '''),
+    html.Br(),
+    html.H4('''
+            1. This application was created as part of an introduction to connecting to a sensor and creating a web application on plotly dash.
+            '''),
+    html.H4('''
+            2. The application includes searching for WT901 sensors, connection, basic settings, graphical output of the converted results and saving them in res.csv.
+        '''),
+    html.H4('''
+            3. Connecting via Bluetooth has a slight delay, so please take your time when using this method (bug: the work of the field for selecting available sensors is not working. Solution: Don't click the Start button right away). USB connection works without problems.
+        '''),
 ])
